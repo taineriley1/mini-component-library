fde9559557065640c3a6ed72593dea85284a456b
Taine Riley <taineriley@ssw.com.au>
2021-12-03 13:28:59 +1000

    Workshop

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,41 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  'small': {
+    height: 8,
+    borderRadius: 4,
+  },
+  'medium': {
+    height: 12,
+    borderRadius: 4,
+  },
+  'large': {
+    height: 24,
+    borderRadius: 8,
+    padding: 4,
+  },
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper size={size} style={SIZES[size]} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
+      <Progress value={value} size={size} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const Progress = styled.div`
+  background-color: ${COLORS.primary};
+  background: linear-gradient(90deg, ${COLORS.primary} 0% ${props => props.value}%, transparent ${props => props.value}% 100%);
+  height: 100%;
+  border-radius: ${props => props.size === 'large' ? '4px' : 'inherit'};
+
+`
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,64 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
       {children}
-    </select>
+      </NativeSelect>
+      <PresentationalSelect>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down"></Icon>
+        </IconWrapper>
+      </PresentationalSelect>
+    </Wrapper>
+    </>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+`;
+
+const PresentationalSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  padding: 12px 16px;
+  padding-right: 52px;
+  white-space: nowrap;
+  color: ${COLORS.gray700};
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  pointer-events: none;
+`
 
 export default Select;
